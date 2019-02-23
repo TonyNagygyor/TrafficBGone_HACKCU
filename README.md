1d0b8fbde626111a16a3f76f71f54e5056e25a90
Liam Warfield <liam.warfield@gmail.com>
2019-02-23 16:45:35 -0700

    GCP is alive

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # TrafficBGone_HACKCU
 A Program to Encourage Alternate Travel Times
+
+Install Request and bottle.
+
+Set an envirnment variabl with the GCP api key:
+
+$ export APIKEY="Insert key here"
--- server.py
+++ server.py
@@ -1,4 +1,18 @@
 from bottle import route, run
+import requests
+import os
+print(os.environ['APIKEY'])
+
+api_key = os.environ['APIKEY']
+origin = 'Golden,CO'
+destination = 'Keystone_Resort'
+
+if api_key == '':
+    print('ERROR NO API KEY')
+
+r = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=' + origin + '&destinations=' + destination + '&departure_time=1551020400&key=' + api_key)
+print(r.text)
+print('hello')
 
 @route('/hello')
 def hello():
