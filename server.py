5677166b7cdaee3dd8261e003dc9e74cd2811dd0
Liam Warfield <liam.warfield@gmail.com>
2019-02-23 17:37:50 -0700

    got the first rendition of the time end point done

--- server.py
+++ server.py
@@ -1,28 +1,34 @@
-from bottle import route, run
+from bottle import route, run, error
 import requests
 import os
-print(os.environ['APIKEY'])
 
-api_key = os.environ['APIKEY']
-origin = 'Golden,CO'
-destination = 'Keystone_Resort'
 
-if api_key == '':
-    print('ERROR NO API KEY')
-
-r = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=' + origin + '&destinations=' + destination + '&departure_time=1551020400&key=' + api_key)
-print(r.text)
-print('hello')
+#These function exptect the request to be valid
+def time_with_trafic(r):
+    print(r.json()['rows'][0]['elements'][0]['duration_in_traffic']['value'])
+def time_normal(r):
+    print(r.json()['rows'][0]['elements'][0]['duration_in_traffic']['value'])
 
 @route('/hello')
 def hello():
     return "Hello World!"
 
-run(host='localhost', port=8080, debug=True)
+@error(505)
+def error505(error):
+    return 'The gerbals are confused'
 
 @post('/get_time')
 def get_time():
     origin = request.forms.get('origin')
     destination = request.forms.get('destination')
     r = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=' + origin + '&destinations=' + destination + '&departure_time=1551020400&key=' + api_key)
-    print(r.text)
+    if(r.json()['status'] != 'OK'):
+        print('warning invalid query')
+        return(error505('invalid query'))
+    time = time_with_trafic(r)
+    
+api_key = os.environ['APIKEY']
+if api_key == '':
+    print('ERROR NO API KEY')
+
+run(host='localhost', port=8080, debug=True)
